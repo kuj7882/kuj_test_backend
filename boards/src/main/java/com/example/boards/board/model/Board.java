04b3884616f9581babd01165f075b3386cfb8c59
youjin0409 <kuh7882@naver.com>
2025-02-26 14:10:09 +0900

    [Feat] swagger추가

---
+++ boards/src/main/java/com/example/boards/board/model/Board.java
@@ -0,0 +1,26 @@
+package com.example.boards.board.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+    private String title;
+    private String content;
+    private String writer;
+
+
+}
---
+++ boards/src/main/java/com/example/boards/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.example.boards.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("게시판")
+                .description("게시판의 AIP 테스트")
+                .version("1.0.0");
+    }
+}
