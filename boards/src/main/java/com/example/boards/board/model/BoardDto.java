e7b4764df88e779305e838753b70085509b247de
youjin0409 <kuh7882@naver.com>
2025-02-26 14:19:43 +0900

    [Feat] 게시판 Dto 작성.

---
+++ boards/src/main/java/com/example/boards/board/model/BoardDto.java
@@ -0,0 +1,39 @@
+package com.example.boards.board.model;
+
+import com.example.boards.board.model.Board;
+import lombok.Builder;
+import lombok.Getter;
+
+public class BoardDto {
+    @Getter
+    public static class BoardReq {
+        private Long idx;
+        private String title;
+        private String content;
+        private String writer;
+
+        public Board toEntity() {
+            return Board.builder()
+                    .title(title)
+                    .content(content)
+                    .writer(writer)
+                    .build();
+        }
+        @Getter @Builder
+        public static class BoardRes {
+            private Long idx;
+            private String title;
+            private String writer;
+            private String content;
+
+            public static BoardRes of(Board entity) {
+                return BoardRes.builder()
+                        .idx(entity.getIdx())
+                        .title(entity.getTitle())
+                        .content(entity.getContent())
+                        .writer(entity.getWriter())
+                        .build();
+            }
+        }
+    }
+}
